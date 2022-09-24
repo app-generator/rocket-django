03868f5854e669403bff276b51eb0acae6d7823b
App Generator <51070104+app-generator@users.noreply.github.com>
2022-09-24 19:59:50 +0300

    Update common.py

--- util/common.py
+++ util/common.py
@@ -7,8 +7,8 @@ class COMMON:
 
     NULL        = None # not set
     NA          = -1   # not set
-    OK          =  0   # all ok
-    ERR         =  1   # not ok
+    OK          =  0   # All good   (unix style)
+    ERR         =  1   # Err bumped (unix style)
     NOT_FOUND   =  2   # file or directory not found
     INPUT_ERR   =  3   # file or directory not found
 
