a62c76908ae38662acdedf3c5c19d142a19d0323
App Generator <51070104+app-generator@users.noreply.github.com>
2022-09-24 20:00:03 +0300

    Update __init__.py

--- util/__init__.py
+++ util/__init__.py
@@ -3,6 +3,7 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 
-from .common  import *
-from .helpers import *
-from .cli     import *
+from .common    import *
+from .helpers   import *
+from .cli       import *
+from .generator import *
