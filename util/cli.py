7e4d0037071d232fc5e26832b3567242cbca694c
App Generator <51070104+app-generator@users.noreply.github.com>
2022-09-24 19:59:59 +0300

    Update cli.py

--- util/cli.py
+++ util/cli.py
@@ -3,5 +3,28 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 
-from .common  import *
+import subprocess
 
+from .common  import *
+from .helpers import *
+
+def exec_cmd( full_cmd ):
+
+    retcode = COMMON.OK
+    stdout  = ''
+    stderr  = ''
+
+    try:
+
+        # create project in src
+        result = subprocess.run( full_cmd.split(' '), check=True)
+
+        retcode = result.returncode 
+        stdout  = result.stdout 
+        stderr  = result.stderr         
+
+    except Exception as e:
+
+        retcode = COMMON.ERR
+
+    return retcode, stdout, stderr
