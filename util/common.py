2a13109563554993f6856a483e2e4a8e844e5b59
App Generator <51070104+app-generator@users.noreply.github.com>
2022-09-25 17:16:57 +0300

    COMMON namespace - added vars

--- util/common.py
+++ util/common.py
@@ -3,14 +3,29 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 
+import os
+
+DIR_ROOT          = os.path.dirname(os.path.dirname(__file__))
+DIR_SRC           = os.path.join(DIR_ROOT    , 'src'  )
+DIR_DJ_CORE       = os.path.join(DIR_SRC     , 'core' )
+
+FILE_DJ_MANAGE    = os.path.join(DIR_SRC     , 'manage.py'  )
+FILE_DJ_SETTINGS  = os.path.join(DIR_DJ_CORE , 'settings.py')
+
 class COMMON:
 
-    NULL        = None # not set
-    NA          = -1   # not set
-    OK          =  0   # All good   (unix style)
-    ERR         =  1   # Err bumped (unix style)
-    NOT_FOUND   =  2   # file or directory not found
-    INPUT_ERR   =  3   # file or directory not found
+    NULL              = None # not set
+    NA                = -1   # not set
+    OK                =  0   # All good   (unix style)
+    ERR               =  1   # Err bumped (unix style)
+    NOT_FOUND         =  2   # file or directory not found
+    INPUT_ERR         =  3   # file or directory not found
+
+    # Settings vars typologies 
+    CFG_VAR_NA        = 10 # Type is undetected
+    CFG_VAR_SIMPLE    = 11 # Ex: SECRET_KEY
+    CFG_VAR_LIST      = 12 # Ex: INSTALLED_APPS, MIDDLEWARE
+    CFG_VAR_MIXED     = 13 # List of Dicts, Ex: AUTH_PASSWORD_VALIDATORS 
 
 # Recover errors for COMMON class
 def errInfo( aErrorCode ):
@@ -22,3 +37,13 @@ def errInfo( aErrorCode ):
     if COMMON.INPUT_ERR  == aErrorCode: return 'Input error'
 
     return str( aErrorCode )
+
+def commonTxt( aCode ):
+
+    if COMMON.CFG_VAR_NA     == aCode: return 'CFG Var unknown typology'
+    if COMMON.CFG_VAR_SIMPLE == aCode: return 'CFG Var SIMPLE'
+    if COMMON.CFG_VAR_LIST   == aCode: return 'CFG Var LIST'
+    if COMMON.CFG_VAR_MIXED  == aCode: return 'CFG Var MIXT (list of dicts)'
+
+    return str( aErrorCode )
+ 
