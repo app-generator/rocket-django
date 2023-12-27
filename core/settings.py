34f7ffd5c9215ca8092a8dfab802177fb73288aa
mahfujur <mahfujul.hoque@heliosinsider.com>
2023-12-27 13:31:40 +0600

    fixed-smtp

--- core/settings.py
+++ core/settings.py
@@ -207,7 +207,16 @@ CELERY_RESULT_SERIALIZER  = 'json'
 
 
 LOGIN_REDIRECT_URL = '/'
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = os.environ.get('EMAIL_PORT', 587)
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', True)
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER',)
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 # ### API-GENERATOR Settings ###
 API_GENERATOR = {
